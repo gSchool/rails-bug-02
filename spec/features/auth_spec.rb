63625cedde812795efa833fb5f651f6bc6bcb775
Charles Painter <cpainter85@gmail.com>
2015-02-20 14:00:21 -0700

    when user registers, they are automatically logged in

--- spec/features/auth_spec.rb
+++ spec/features/auth_spec.rb
@@ -16,4 +16,15 @@ feature 'Auth' do
     expect(page).to have_content("user@example.com")
   end
 
+  scenario 'When user registers, should be logged in automatically' do
+    visit root_path
+    click_on 'Register'
+    fill_in 'Name', with: 'Joe Smith'
+    fill_in 'Email', with: 'joesmith@email.com'
+    fill_in 'Password', with: 'jsmithpassword'
+    fill_in 'Confirm', with: 'jsmithpassword'
+    within('.registration-form') { click_on 'Register'}
+    expect(page).to have_content('You are logged in successfully.')
+  end
+
 end
