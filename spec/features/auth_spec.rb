cd924fcdf1aeefe4e6ddfa4d9a2bf9f79b58e502
Peter Furey <prfurey@gmail.com>
2015-02-20 16:06:29 -0700

    Used rspec to fix bug in registration

--- spec/features/auth_spec.rb
+++ spec/features/auth_spec.rb
@@ -16,4 +16,46 @@ feature 'Auth' do
     expect(page).to have_content("user@example.com")
   end
 
+  scenario 'Users can go to register page' do
+    visit root_path
+    within("div.auth") { click_on "Register" }
+    expect(page).to have_content("Register")
+  end
+
+  scenario 'Users can go to register page' do
+    visit new_registration_path
+    within(".registration-form") { click_on "Register" }
+    expect(page).to have_content("Password can't be blank")
+  end
+
+  scenario 'Users can register' do
+    
+    visit new_registration_path
+    within(".registration-form") { click_on "Register" }
+    expect(page).to have_content("Password can't be blank")
+
+    fill_in "Name", with: "user"
+    fill_in "Email", with: "user@example.com"
+    fill_in "Password", with: "password"
+    fill_in "Confirm", with: "password"
+    within(".registration-form") { click_on "Register" }
+    expect(page).to have_no_content("Password can't be blank")
+
+  end
+
+  scenario 'User gets to new page opened after login registration' do
+
+    visit new_registration_path
+    within(".registration-form") { click_on "Register" }
+    expect(page).to have_content("Password can't be blank")
+
+    fill_in "Name", with: "user"
+    fill_in "Email", with: "user@example.com"
+    fill_in "Password", with: "password"
+    fill_in "Confirm", with: "password"
+    within(".registration-form") { click_on "Register" }
+    expect(page).to have_content("You are logged in successfully.")
+
+  end
+
 end
