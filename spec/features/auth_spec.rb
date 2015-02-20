811efa88170d3ee586c8d272dd0d39ec99d9d94b
Tristan Rubadeau <tristan@rubadeau.com>
2015-02-20 13:22:20 -0700

    wrote failing test

--- spec/features/auth_spec.rb
+++ spec/features/auth_spec.rb
@@ -16,4 +16,16 @@ feature 'Auth' do
     expect(page).to have_content("user@example.com")
   end
 
+  scenario 'After user logs in they are directed to the root' do
+    created_user email: "user.example.com"
+
+    visit root_path
+    within(".signin-form") { click_on "Login" }
+
+    fill_in "Email", with: "user@example.com"
+    fill_in "Password", with: "password"
+    within(".signin-form") { click_on "Login"}
+    expect(page).to have_content("You are logged in successfully.")
+
+  end
 end
