c6ed64358dbf9e9f59b67db7c2a2b4239a5ee78a
Alex <alexandralaudon@gmail.com>
2015-02-23 13:48:28 -0700

    Completed exercise

--- spec/features/auth_spec.rb
+++ spec/features/auth_spec.rb
@@ -16,4 +16,31 @@ feature 'Auth' do
     expect(page).to have_content("user@example.com")
   end
 
+  scenario 'Users can register and automatically login after registration' do
+    visit root_path
+
+    click_link "Register"
+
+    within(".registration-form") {expect(page).to have_content("Register")}
+
+    fill_in "Name", with: "Mahamdi Gandhi"
+    fill_in "Email", with: "peace@hotmail.com"
+    fill_in "Password", with: "lovenotwar"
+    fill_in "Confirm", with: "lovenotwar"
+
+    within(".registration-form") {click_on "Register"}
+    expect(page).to have_content("Welcome peace@hotmail.com")
+
+    # expect(page).to have_xpath?('/registrations/new')
+    #
+    # fill_in "Name", with: "John Smith"
+    # fill_in "Email", with: "johnsmith@yahoo.com"
+    # fill_in "Password", with: "johnsmith"
+    # fill_in "Confirm", with: "johnsmith"
+    # click_button('Register')
+    # expect(page).to have_content("Welcome John Smith")
+    # expect(page).to have_contect("You are logged in successfully.")
+
+  end
+
 end
