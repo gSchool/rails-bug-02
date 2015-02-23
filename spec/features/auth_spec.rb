e43110c3e2af58d8f00a305700bee7a7aa9465df
Annie Lydens <aelydens@gmail.com>
2015-02-23 03:33:16 -0700

    Add spec for registration

--- spec/features/auth_spec.rb
+++ spec/features/auth_spec.rb
@@ -16,4 +16,14 @@ feature 'Auth' do
     expect(page).to have_content("user@example.com")
   end
 
+  scenario 'Users can register' do
+    visit new_registration_path
+    fill_in "Name", with: "Annie Test"
+    fill_in "Email", with: "aelydens@test.com"
+    fill_in "Password", with: "password"
+    fill_in "Confirm", with: "password"
+    within("form") { click_on "Register" }
+
+    expect(page).to have_content("aelydens@test.com")
+  end
 end
