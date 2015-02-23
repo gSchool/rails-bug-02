66ef92cd0c7fd33f2e6091669308fb72fc5f5585
Kate Kilian Vovk <kathrynjkilian@gmail.com>
2015-02-23 13:48:31 -0700

    Completed exercise

--- spec/features/auth_spec.rb
+++ spec/features/auth_spec.rb
@@ -16,4 +16,26 @@ feature 'Auth' do
     expect(page).to have_content("user@example.com")
   end
 
+  scenario "Users can register and are automatically logged in" do
+    create_user email: "user@example.com"
+
+    visit root_path
+    click_link "Register"
+
+    within(".registration-form") { click_on "Register" }
+
+    fill_in "Name", with: "Mickey Mouse"
+    fill_in "Email", with: "mickey@mouse.com"
+    fill_in "Password", with: "password"
+    fill_in "Confirm", with: "password"
+
+    within(".registration-form") { click_on "Register" }
+
+    expect(page).to have_content("Welcome mickey@mouse.com")
+
+
+
+  end
+
+
 end
