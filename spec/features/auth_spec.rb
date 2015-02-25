e410c3cec33ca45317af21871011cb613f5d0200
Jeffrey Mince <jeffrey.mince@gmail.com>
2015-02-25 15:14:38 -0700

    Wrote failing test & fixed bug-app 2

--- spec/features/auth_spec.rb
+++ spec/features/auth_spec.rb
@@ -14,6 +14,19 @@ feature 'Auth' do
     fill_in "Password", with: "password"
     within(".signin-form") { click_on "Login" }
     expect(page).to have_content("user@example.com")
+
+
+  end
+
+  scenario 'Users are redirected to session' do
+
+    visit new_registration_path
+    fill_in "Name", with: "Joe"
+    fill_in "Email", with: "joe@joe.com"
+    fill_in "Password", with: "joe"
+    fill_in "Confirm", with: "joe"
+    within(".registration-form") { click_on "Register" }
+    expect(page).to have_content("You are logged in successfully")
   end
 
 end
