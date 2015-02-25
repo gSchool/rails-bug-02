d1f7ea52e0450b1fd821cefcd2039acacc36234e
Danmorrisett <danmorrisett@gMAIL.com>
2015-02-25 14:13:52 -0700

    Finished rails-bug-02

--- spec/features/auth_spec.rb
+++ spec/features/auth_spec.rb
@@ -16,4 +16,17 @@ feature 'Auth' do
     expect(page).to have_content("user@example.com")
   end
 
+  scenario 'When users register, they login in' do
+    visit root_path
+    click_on "Register"
+    expect(page).to have_content("Register")
+    fill_in "Name", with: "Steve"
+    fill_in "Email", with: "Steve@crazy.com"
+    fill_in "Password", with: "123"
+    fill_in "Confirm", with: "123"
+    within(".registration-form") { click_on "Register" }
+    expect(page).to have_content("You are logged in successfully.")
+  end
+
+
 end
