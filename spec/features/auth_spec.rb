ac8884e971c8a7a6a2aad25e63c6c68f0b136ff0
Kyle DeFauw <kyle.defauw@gmail.com>
2015-02-25 15:36:56 -0700

    redirect registration page to current user

--- spec/features/auth_spec.rb
+++ spec/features/auth_spec.rb
@@ -16,4 +16,15 @@ feature 'Auth' do
     expect(page).to have_content("user@example.com")
   end
 
+  scenario 'User is redirected to profile page after registration' do
+    visit new_registration_path
+
+    fill_in "Name", with: "Joe Mama"
+    fill_in "Email", with: "joe@mama.com"
+    fill_in "Password", with: "123"
+    fill_in "Confirm", with: "123"
+    within(".registration-form") { click_on "Register"}
+    expect(page).to have_content("You are logged in successfully.")
+  end
+
 end
